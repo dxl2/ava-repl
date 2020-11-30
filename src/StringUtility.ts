2b342a355c4106339c7cc0906874c0da8bfdd4ea
theactiveactor <theactiveactor@gmail.com>
2020-11-30 03:36:30 +0100

    Support quoted parameters (that include spaces)

--- src/StringUtility.ts
+++ src/StringUtility.ts
@@ -3,12 +3,37 @@ export class StringUtility {
         let parts = s.trim().split(sep)
         let out:string[] = []
 
-        // Remove quotes in case they're pasted in from JSON output
-        for (let part of parts) {
+        // Keep quoted text together; remove quotes
+        let glu = []
+        for (let part of parts) {  
             if (part.length > 2 && part[0] == "\"" && part[part.length - 1]=="\"") {
                 part = part.slice(1, -1)
+                out.push(part)
+            } 
+            else if (part.endsWith("\"") || part == "\"") {
+                if (part.length > 1) {
+                    glu.push(part.slice(0, -1))                    
+                }
+
+                if (glu.length) {
+                    out.push(glu.join(" "))
+                    glu = []
+                }                
             }
-            out.push(part)
+            else if (part.startsWith("\"")) {
+                if (part.length > 1) {                    
+                    glu.push(part.substring(1))
+                }
+            } else if (glu.length) {
+                glu.push(part)
+            } else
+            {
+                out.push(part)
+            }
+        }
+
+        if (glu.length) {
+            out.push(glu.join(" "))
         }
 
         return out
